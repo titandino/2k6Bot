f818483d10671b47cb28886cebb0ca622a997138
Trenton Kress <trenton.kress@gmail.com>
2016-06-28 00:45:30 -0700

    mod ali checker added :^)

--- src/bot/scripts/Script.java
+++ src/bot/scripts/Script.java
@@ -2,20 +2,29 @@ package bot.scripts;
 
 import java.awt.Graphics;
 
+import bot.Bot;
+
 public abstract class Script implements Runnable {
-	
+
 	public boolean started = false;
 	public String[] args;
 
 	@Override
 	public void run() {
 		if (!started) {
-			while(!started) {
+			while (!started) {
 				started = onStart();
 			}
 		}
+		try {
+			if (Bot.getPlayerByName("mod ali") != null)
+				Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public abstract boolean onStart();
+
 	public abstract void onRepaint(Graphics g);
 }
